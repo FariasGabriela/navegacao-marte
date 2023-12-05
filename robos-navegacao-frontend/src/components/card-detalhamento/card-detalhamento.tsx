8695d1dac88cf7feb281adb1af2bfe5f3f3b0fbc
FariasGabriela <gabrielafarias@alunos.utfpr.edu.br>
2023-12-05 19:14:10 -0300

    Ajuste no card de legenda

--- robos-navegacao-frontend/src/components/card-detalhamento/card-detalhamento.tsx
+++ robos-navegacao-frontend/src/components/card-detalhamento/card-detalhamento.tsx
@@ -9,7 +9,7 @@ function CardDetalhamento() {
           <div className="bold">{"L ou > :      "} </div> Robô p/ Esquerda
         </div>
         <div className="label-instrucao">
-          <div className="bold">{"L ou < :      "} </div> Robô p/ Direita
+          <div className="bold">{"R ou < :      "} </div> Robô p/ Direita
         </div>
         <div className="label-instrucao">
           <div className="bold">{"M ou > Espaço:"} </div> Robô Andar
