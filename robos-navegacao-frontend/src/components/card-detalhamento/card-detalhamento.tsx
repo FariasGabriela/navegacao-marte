6da8ac18a60410de1402436bbf33a4d71e2f7945
FariasGabriela <gabrielafarias@alunos.utfpr.edu.br>
2023-12-05 19:00:59 -0300

    Implementado card com detalhamento de como usar os comandos

---
+++ robos-navegacao-frontend/src/components/card-detalhamento/card-detalhamento.tsx
@@ -0,0 +1,22 @@
+import './card-detalhamento.css'
+
+function CardDetalhamento() {
+  return (
+    <div className="card-coordenadas">
+      <div>Comandos</div>
+      <div>
+        <div className="label-instrucao">
+          <div className="bold">{"L ou > :      "} </div> Robô p/ Esquerda
+        </div>
+        <div className="label-instrucao">
+          <div className="bold">{"L ou < :      "} </div> Robô p/ Direita
+        </div>
+        <div className="label-instrucao">
+          <div className="bold">{"M ou > Espaço:"} </div> Robô Andar
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CardDetalhamento;
